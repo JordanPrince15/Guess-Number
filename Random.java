18f07b70532f8acc17b43547906b5f3a9afaca64
Jordan Prince <jordanmikha10@gmail.com>
2024-06-17 12:30:25 +0200

    Update Random.java

--- Random.java
+++ Random.java
@@ -3,10 +3,10 @@ import java.util.Scanner; // Import the scanner classs
 class Random{ // This is the random class
     public static void main(String[] args) { // Initialize th3e class
         @SuppressWarnings("resource")
-        Scanner myObject = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         System.out.println("Enter a random number");
 
-        String RandomNum = myObject.nextLine();
+        String RandomNum = userInput.nextLine();
         int random = (int) (Math.random() * 10);
         if (Integer.parseInt(RandomNum) == random) {
             System.out.println("You have won");
